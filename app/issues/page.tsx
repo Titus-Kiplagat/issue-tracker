ed68ddb51e93c78893acd9a33439f9d77abb5fd2
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-08 15:04:35 +0300

    Build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuePage = () => {
 	return (
-		<Button>New Issue</Button>
+		<Button><Link href="/issues/new">New Issue</Link></Button>
 	)
 }
 
