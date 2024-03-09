050242f2061869f66330cb5b018169070dd96bc2
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-09 20:17:11 +0300

    Build custome styled link

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import { prisma } from "../../server/db/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActionsBar from "../components/IssueActionsBar";
-import Link from "next/link";
+import Link from '../components/Link';
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
