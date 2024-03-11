477ec8e82061c3c0e9489070f53ba4a74f76830a
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-11 09:53:13 +0300

    Apply the SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { IssueStatusBadge } from "@/app/components";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import Markdown from 'react-markdown';
 import { prisma } from "../../../server/db/client";
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from "next/link";
+import EditIssueButton from "../_components/EditIssueButton";
+import IssueDetails from "../_components/IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -18,22 +16,12 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   if (!issueById) notFound();
 
   return (
-    <Grid columns={{ initial: '1', md: '2' }} gap="3">
-    <Box className="space-y-3">
-      <Heading>{issueById.title}</Heading>
-      <Flex gap="3">
-        <IssueStatusBadge status={issueById.status} />
-        <Text>{issueById.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="mt-4 prose">
-        <Markdown>{issueById.description}</Markdown>
-      </Card>
+    <Grid columns={{ initial: "1", md: "2" }} gap="3">
+      <Box className="space-y-3">
+        <IssueDetails issue={issueById} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issueById.id}/edit`}>Edit Button</Link>
-        </Button>
+        <EditIssueButton issueId={issueById.id} />
       </Box>
     </Grid>
   );
