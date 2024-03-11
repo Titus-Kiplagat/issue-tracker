86320a02975db649cbec26c983fae22b978aa902
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-11 15:40:08 +0300

    Improve user experience. Add spinner when deleting. Also disable delete button when deleting.

--- app/issues/_components/DeleteIssueButton.tsx
+++ app/issues/_components/DeleteIssueButton.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -6,13 +7,16 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
   const deleteIssue = async () => {
     try {
+      setDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/api/issues");
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   };
@@ -37,8 +41,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button variant="solid" color="red" onClick={deleteIssue}>
+              <Button disabled={isDeleting} variant="solid" color="red" onClick={deleteIssue}>
                 Delete issue
+                {isDeleting && <Spinner />}
               </Button>
             </AlertDialog.Action>
           </Flex>
