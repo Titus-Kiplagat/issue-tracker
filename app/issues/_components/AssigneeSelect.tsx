cc576908b5f68de715270c824edbe1980e42f764
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-03-14 18:03:48 +0300

    Add toast notification and refactor the component

--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -5,31 +5,29 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    isLoading,
-    error,
-    data: users,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    // staleTime: 1000 * 60 * 60 * 24,
-    retry: 3,
-  });
+  const { isLoading, error, data: users } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId || null,
+      })
+      .catch(() =>
+        toast.error("Failed to update issue", { position: "top-right" })
+      );
+  };
+
   return (
     <Select.Root
       defaultValue={issue.assignedToUserId || ""}
-      onValueChange={(userId) => {
-        axios.patch(`/api/issues/${issue.id}`, {
-          assignedToUserId: userId || null,
-        });
-      }}
+      onValueChange={assignIssue}
     >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
@@ -46,5 +44,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </Select.Root>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 1000 * 60 * 60 * 24,
+    retry: 3,
+  });
 
 export default AssigneeSelect;
